b40eb9566597f45d42dd6b8d0183cfa2955f45eb
penghuihui <phh2008@vip.qq.com>
2020-07-16 18:10:15 +0800

    init

---
+++ camunda-bpm-swagger/README.md
@@ -0,0 +1,10 @@
+## github:https://github.com/camunda/camunda-bpm-swagger
+
+## 跳转 doc页面
+http://localhost:8080/webjars/swagger-ui/3.1.4/index.html?docExpansion=false&url=/swagger.json
+
+
+
+
+
+
---
+++ camunda-bpm-swagger/src/main/java/com/example/CamundaSwaggerApplication.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+/**
+ * 描述
+ *
+ * @author phh
+ * @version V1.0
+ * @date 2020/7/16
+ */
+@SpringBootApplication
+@EnableProcessApplication
+public class CamundaSwaggerApplication {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaSwaggerApplication.class, args);
+    }
+
+
+    @EventListener
+    public void run(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("Process_1");
+
+    }
+
+}
